1d201d68e75e9815ad1f6fb0323ef480aacec440
Updesh Kumar <168581762up-ydv@users.noreply.github.com>
2024-06-09 16:00:28 +0530

    app completed

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
+let saveEl=document.getElementById("save-el")
+let countEl=document.getElementById("count-el")
 let count=0;
-//let countel=document.getElementById("")
-function increment(){
-   
-    count=count+1
 
-   document.getElementById("count-el").innerText=count
+
+function increment(){
+    count=count+1
+    countEl.textContent=count
+}
+function save(){
+    let countString=count + " - "
+    saveEl.textContent+=countString
+    countEl.textContent=0
+    count=0
+
 }
